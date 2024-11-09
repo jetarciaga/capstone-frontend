870df09d93db8a9530cc7389528e11564daeddde
Jethro Arciaga <jethro@openarchitectsk12.com>
2024-11-09 22:23:24 +0800

    Added api call to retrieve requirements
    Added feature onChange when barangay document is selected
    it will also update and display the necessary requirements

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -19,18 +19,70 @@ const Appointment = () => {
     []
   );
 
+  const [documents, setDocuments] = useState([]);
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      try {
+        const response = await api.get("documents/");
+        setDocuments(response.data);
+      } catch (error) {
+        console.error("Error retrieving documents", error);
+      }
+    };
+    fetchDocuments();
+  }, []);
+
+  const [selectedDocument, setSelectedDocument] = useState([]);
+  const handleSelectedDocument = async (event) => {
+    setSelectedDocument(event.target.value);
+  };
+
+  const [requirements, setRequirements] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(`requirements/${selectedDocument}/`)
+      .then((response) => setRequirements(response.data))
+      .catch((error) => console.error(error));
+  }, [selectedDocument]);
+
   return (
     <div className="appointment-container">
       <Calendar setDate={setAppointmentDate} setTime={setTimeslotsFromDate} />
-      <form onSubmit={handleSubmit}>
-        <input type="date" value={appointmentDate.toISOString().slice(0, 10)} />
-        <label for="time">Choose available time:</label>
+      <form className="schedule-form" onSubmit={handleSubmit}>
+        <label htmlFor="document">Choose Document:</label>
+        <select id="document" name="document" onChange={handleSelectedDocument}>
+          <option value="" disabled>
+            Choose Document
+          </option>
+          {[...documents].map((document, index) => (
+            <option key={index} value={document.name} details={document}>
+              {document.name}
+            </option>
+          ))}
+        </select>
+        <div className="requirements">
+          <ul>
+            {requirements &&
+              requirements.map((value, index) => (
+                <li key={index}>{value.name}</li>
+              ))}
+          </ul>
+        </div>
+        <input
+          type="date"
+          value={appointmentDate.toISOString().slice(0, 10)}
+          onChange={() => {}}
+        />
+        <label htmlFor="time">Choose available time:</label>
         <select id="time" name="time">
           <option value="" disabled>
             Choose time
           </option>
           {[...timeslots].map((time, index) => (
-            <option key={index}>{time}</option>
+            <option key={index} value={time}>
+              {time}
+            </option>
           ))}
         </select>
       </form>
