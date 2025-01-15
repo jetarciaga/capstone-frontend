080bef221577c13a251477f44f08cffadfdfceed
Jethro Arciaga <jet.arciaga@gmail.com>
2025-01-15 20:53:13 +0800

    Added content for NotFound page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./NotFound.scss";
 
 const NotFound = () => {
   const location = useLocation();
@@ -7,14 +8,27 @@ const NotFound = () => {
   const underConstruction = ["/careers", "/announcements"];
 
   const isUnderConstruction = underConstruction.includes(location.pathname);
+  const pageName = location.pathname.slice(1);
+
+  const message = {
+    careers: "No available opening for now.",
+    announcements: "No announcements.",
+  };
+
+  const Template = (
+    <>
+      <h1>{pageName.toUpperCase()}</h1>
+      <hr />
+      <div className="page-body">
+        <h2>{message[`${pageName}`]}</h2>
+        <h3>Check again tomorrow</h3>
+      </div>
+    </>
+  );
 
   return (
-    <div>
-      {isUnderConstruction ? (
-        <h1>{location.pathname.slice(1, -1).toUpperCase()}</h1>
-      ) : (
-        <h1>Not Found</h1>
-      )}
+    <div className="page-container">
+      {isUnderConstruction ? Template : <h1>Not Found</h1>}
     </div>
   );
 };
