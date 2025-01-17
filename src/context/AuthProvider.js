bd709959206b97fd06578ff8da3976df26c8b64a
Jethro Arciaga <jet.arciaga@gmail.com>
2025-01-17 12:15:09 +0800

    comment csrfToken

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -57,7 +57,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    fetchCsrfToken();
+    // fetchCsrfToken();
     if (accessToken) {
       fetchUserDetails(accessToken);
     }
